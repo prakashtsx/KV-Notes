ccc9c220b36a5e83198af42353846a1a413d6cd9
KrrishVardhan <vyaskrrishvardhan@gmail.com>
2025-12-23 18:45:04 +0530

    adding subject names

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { Button } from "@/components/ui/8bit/button"
 import {
   Card,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
+  CardContent,
+  CardAction
 } from "@/components/ui/8bit/card"
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
@@ -13,35 +16,41 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 const pdfList = [
   {
     id: 1,
-    name: "Assigment_1",
-    pages: 56,
+    name: "Data Science",
+    pages: "[6CS4-01]",
     url: "/pdfs/document1.docx"
   },
   {
     id: 2,
-    name: "Assigment_2",
-    pages: 32,
+    name: "Deep Learning",
+    pages: "[6CS4-02]",
     url: "/pdfs/document1.docx"
   },
   {
     id: 3,
-    name: "Assigment_3",
-    pages: 10,
+    name: "CIA",
+    pages: "[6CS4-03]",
     url: "/pdfs/document1.docx"
   },
   {
     id: 4,
-    name: "Assigment_4",
-    pages: 34,
+    name: "Computer Graphics",
+    pages: "[6CS4-04]",
     url: "/pdfs/document1.docx"
-  }]
-const handleView = (url: string): void => {
-  window.open(url, '_blank');
-};
-function DownloadIcon() {
-  return (
-    <svg width="30px" height="30px" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg"><g fill="none" className="nc-icon-wrapper" stroke-linejoin="miter" stroke-linecap="butt"><path d="M15 7H15.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M15 3H15.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M7 11H7.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M11 15H11.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M15 19H15.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M19 15H19.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M23 11H23.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M15 11H15.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M15 15H15.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M7 27H7.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M11 27H11.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M15 27H15.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M19 27H19.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M23 27H23.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M27 23H27.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M3 23H3.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M27 19H27.01" stroke="rgba(255, 255, 255, 0.23137254901960785)" stroke-width="4" stroke-linecap="square" data-arcade-2="on"></path><path d="M15 15H15.01" stroke="rgba(255, 255, 255, 1)" stroke-width="4" stroke-linecap="square" data-arcade-1="on"></path><path d="M19 11H19.01" stroke="rgba(255, 255, 255, 0.23137254901960785)" stroke-width="4" stroke-linecap="square" data-arcade-2="on"></path><path d="M11 11H11.01" stroke="rgba(255, 255, 255, 0.23137254901960785)" stroke-width="4" stroke-linecap="square" data-arcade-2="on"></path><path d="M3 19H3.01" stroke="rgba(255, 255, 255, 0.23137254901960785)" stroke-width="4" stroke-linecap="square" data-arcade-2="on"></path><path d="M3 27H3.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M27 27H27.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M23 23H23.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M19 23H19.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M15 23H15.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M11 23H11.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M7 23H7.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M7 19H7.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M7 15H7.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M3 15H3.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M3 11H3.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M3 7H3.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M3 3H3.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M7 3H7.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M7 7H7.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M11 3H11.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M11 7H11.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M11 19H11.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M19 19H19.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M23 19H23.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M23 15H23.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M27 15H27.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M27 11H27.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M27 7H27.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M27 3H27.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M23 3H23.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M23 7H23.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M19 7H19.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path><path d="M19 3H19.01" stroke="#1c1f2100" stroke-width="4" stroke-linecap="square" data-arcade-3="on"></path></g></svg>)
-}
+  },
+  {
+    id: 5,
+    name: "Distributed Algo",
+    pages: "[6CS4-05]",
+    url: "/pdfs/document1.docx"
+  },
+  {
+    id: 6,
+    name: "Cloud Computing",
+    pages: "[6CS4-11]",
+    url: "/pdfs/document1.docx"
+  },
+]
 function App() {
   return (
     <>
@@ -50,22 +59,25 @@ function App() {
       </div>
       <main className="flex"
         style={{
-          backgroundImage: `radial-gradient(circle at 0.5px 0.5px, rgba(255,255,255,0.2) 0.5px, transparent 0px)`,
+          backgroundImage: `radial-gradient(circle at 0.5px 0.5px, rgba(255,255,255,0.3) 0.5px, transparent 0px)`,
           backgroundSize: "8px 8px"
         }}
       >
-        <article className="flex md:p-10 p-7 flex-col gap-5 min-h-screen"
+        <article className="flex md:p-10 p-7 flex-wrap gap-15 min-h-screen"
         >
           {pdfList.map((pdf) => (
-            <Card key={pdf.id} className="flex flex-row justify-between md:p-5 p-1 items-center md:w-2xl">
+            <Card className="w-full max-w-sm h-fit">
               <CardHeader>
-                <CardTitle className="">{pdf.name}</CardTitle>
-                <CardDescription className="text-xs">{pdf.pages} pages</CardDescription>
+                <CardTitle>{pdf.name}</CardTitle>
+                <CardDescription>
+                  {pdf.pages}
+                </CardDescription>
               </CardHeader>
-              <Button className="cursor-pointer" onClick={() => handleView(pdf.url)} variant="secondary">
-                <DownloadIcon />
-                <p className="hidden md:block">Download</p>
-              </Button>
+              <CardFooter className="flex-col">
+                <Button variant="outline" className="w-full cursor-pointer">
+                  View
+                </Button>
+              </CardFooter>
             </Card>
           ))}
         </article>
